32fb24bd7bd7513cc605c2b2ca72f0e699a3db8b
Dominic Hains <contact.dominichains@gmail.com>
2021-05-24 12:35:12 -0400

    feat(all): Get data from Ergast API

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+
 import './App.css';
 
+const fetchDriverData = async (season, driver) => {
+  const res = await axios.get(
+    `http://ergast.com/api/f1/${season}/drivers/${driver}/results.json`
+  );
+  return JSON.stringify(res);
+};
+
 function App() {
+  const [data, setData] = useState('');
+  const [driver, setDriver] = useState('vettel');
+  const [season, setSeason] = useState('current');
+
+  useEffect(() => {
+    fetchDriverData(season, driver).then((data) => setData(data));
+  }, [season, driver]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='app'>
+      <pre>{data}</pre>
     </div>
   );
 }
